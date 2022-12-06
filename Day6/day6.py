69e3579fb81bd73e7211f01b1ed8b2d851806bb0
Keydouu <nmro1@hotmail.fr>
2022-12-06 22:54:10 +0100

    Day 6

--- Day6/day6.py
+++ Day6/day6.py
@@ -5,15 +5,16 @@ i=0
 read=""
 foundPart1=False
 while True:
+    c=input[i]
     if not foundPart1:
         if len(read)==4:
             foundPart1=True
             print("Part 1 : "+str(i))
-    if input[i] in read:
-        read=read[(read.index(input[i])+1):len(read)]
-        read+=input[i]
+    if c in read:
+        read=read[(read.index(c)+1):len(read)]
+        read+=c
     else:
-        read+=input[i]
+        read+=c
         if len(read)==14:
             break
     i+=1
