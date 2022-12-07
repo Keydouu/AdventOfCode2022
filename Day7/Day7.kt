6bd490e2707b0a7f5a9c66050c7aefb1f776667e
Keydouu <nmro1@hotmail.fr>
2022-12-07 14:24:28 +0100

    Day 7

---
+++ Day7/Day7.kt
@@ -0,0 +1,83 @@
+import java.io.File
+import java.io.InputStream
+
+var mainDirectory:MyDirectory = MyDirectory("/",0)
+var currentDirectory= mainDirectory
+
+var allDirectories = mutableListOf<MyDirectory>()
+
+fun main(){
+    val inputStream: InputStream = File("Input.txt").inputStream()
+	inputStream.bufferedReader().useLines {
+        lines -> lines.forEach {
+            var splitedInput=it.split(' ')
+            if(splitedInput[0]=="$"){//command
+                if(splitedInput[1]=="cd"){//cd
+                    if(splitedInput[2]=="..")//..
+                        currentDirectory.goToParrentDirectory()
+                    else
+                        currentDirectory.goToSubDirectory(splitedInput[2])
+                }
+            }
+            else{
+                var newDir:MyDirectory
+                if(splitedInput[0]=="dir")
+                    newDir=MyDirectory(splitedInput[1], 0)
+                else
+                    newDir=MyDirectory(splitedInput[1], splitedInput[0].toLong(), "file")
+                newDir.parrentDirectory=currentDirectory
+                currentDirectory.addSubFile(newDir)
+            }
+        }
+    }
+    var total:Long=0
+    var directoryToDelete=mainDirectory
+    var requiredSpace=(30000000+mainDirectory.size-70000000)
+    println("required space : "+requiredSpace)
+    for(entry in allDirectories){
+        if(entry.type=="dir"){
+            if ((entry.size <= 100000))
+                total+=entry.size
+            if((entry.size>requiredSpace)and(entry.size<directoryToDelete.size))
+                directoryToDelete=entry
+        }
+    }
+    println("Part 1 : "+total)
+    print("Part 2 : "+directoryToDelete.size)
+}
+
+class MyDirectory(val name: String, var size: Long, val type: String="dir"){
+    var parrentDirectory:MyDirectory=mainDirectory
+    var subFiles = mutableMapOf<String, MyDirectory>()
+    fun addSubFile(newDirectory: MyDirectory){// considered files as directory because having both in one list is a pain in the ass
+        if(subFiles.get(newDirectory.name)==null){
+            subFiles.put(newDirectory.name,newDirectory)
+            if(newDirectory.type=="file")
+                this.addSize(newDirectory.size)
+            else
+                allDirectories.add(newDirectory)
+        }
+    }
+    fun addSize(extraSize:Long){
+        this.size+=extraSize
+        if(this.name!="/")//avoiding infinite loop because main directory's parrent directory is itself
+            parrentDirectory.addSize(extraSize)
+    }
+    fun goToParrentDirectory(){
+        //if(currentDirectory==this) Always true as long as input is correct
+            currentDirectory=parrentDirectory
+    }
+    fun goToSubDirectory(subName:String){
+        if(subName!="/"){
+            var newDir=subFiles.get(subName)
+            if(newDir!=null)
+                currentDirectory=newDir
+            else{
+                newDir=MyDirectory(subName, 0)
+                this.addSubFile(newDir)
+                newDir.parrentDirectory=this
+                currentDirectory=newDir
+            }
+        }
+    }
+}
